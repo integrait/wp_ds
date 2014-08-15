4bb170643b8102ea4b96ee18557c357e954d2c41
Sodimu Segun <princesegzy01@yahoo.com>
2014-08-15 16:05:17 +0100

    finish index

--- wp-content/themes/sistina/drugstoc/functions.php
+++ wp-content/themes/sistina/drugstoc/functions.php
@@ -14,4 +14,15 @@ function yit_copyright_DRUGSTOC(){
 	endwhile;
 	} 
 
+
+	function add_additional_css() {
+	//wp_enqueue_style( 'style-name', get_stylesheet_uri() );
+	//wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
+	//wp_enqueue_script( 'script-name', get_template_directory_uri() . '/css/additionalCss.css', array(), '1.0.0', true );
+	//wp_enqueue_style( 'style-name', get_stylesheet_uri() );
+	wp_enqueue_style('style-name', get_template_directory_uri() . '/css/additionalCss.css' );
+}
+
+add_action( 'wp_enqueue_scripts', 'add_additional_css' );
+
 ?>
