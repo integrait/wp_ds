17dc941d2e5177b4c2467b4697c4c557e2daa270
calebcegan <calebte2006@yahoo.com>
2014-08-21 16:35:43 +0100

    Changed default item thumbnail

--- wp-content/plugins/woocommerce/includes/wc-product-functions.php
+++ wp-content/plugins/woocommerce/includes/wc-product-functions.php
@@ -279,7 +279,12 @@ add_filter( 'post_type_link', 'wc_product_post_type_link', 10, 2 );
  * @return string
  */
 function wc_placeholder_img_src() {
-	return apply_filters( 'woocommerce_placeholder_img_src', WC()->plugin_url() . '/assets/images/placeholder.png' );
+	global $wpdb;
+
+	$result = $wpdb->get_row("SELECT meta_value FROM {$wpdb->postmeta} WHERE meta_key = 'default-item-thumbnail'");
+	$image_src = ($result)? $result->meta_value: WC()->plugin_url() . '/assets/images/placeholder.png';
+
+	return apply_filters( 'woocommerce_placeholder_img_src', $image_src );
 }
 
 /**
