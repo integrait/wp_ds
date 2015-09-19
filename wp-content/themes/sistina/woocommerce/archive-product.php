d71b35ec6788b4e013f668cadda25709f12900db
Git User <git@localhost>
2015-09-19 09:36:35 -0500

    Data privacy fix - item 2

--- wp-content/themes/sistina/woocommerce/archive-product.php
+++ wp-content/themes/sistina/woocommerce/archive-product.php
@@ -12,11 +12,45 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly  
 
 // get the style
-global $woocommerce_loop, $blog_id;
+global $woocommerce_loop, $blog_id, $wpdb;
+
 $cookie_shop_view = 'yit_' . get_template() . ( is_multisite() ? '_' . $blog_id : '' ) . '_shop_view';
 $woocommerce_loop['view'] = isset( $_COOKIE[ $cookie_shop_view ] ) ? $_COOKIE[ $cookie_shop_view ] : yit_get_option( 'shop-view', 'grid' );
 
-get_header('shop'); ?>
+get_header('shop'); 
+
+$url = explode("/", $_SERVER['REQUEST_URI']);
+$manu_slug = in_array('manufacturer', $url)? $url[array_search('manufacturer', $url) + 1]: '';
+$manu_slug = trim($manu_slug);
+
+if($manu_slug != ''){ 
+ 
+  	$user = current(get_users(
+  		array(  
+			'role'        => 'manufacturer',
+			'meta_key'    => 'manufacturer_slug',
+			'meta_value'  => $manu_slug,
+			'meta_compare'=> '=',
+			'number' 	  => '1'
+		)
+  	));   
+
+	// Create Variables for info box
+	$address = get_user_meta($user->ID, 'billing_address_1', true).",".get_user_meta($user->ID, 'billing_address_2', true).",".get_user_meta($user->ID, 'billing_city', true).",".get_user_meta($user->ID, 'billing_state', true);
+	$institution = get_user_meta($user->ID, 'institution', true); 
+	$user_img_url = get_cupp_meta($user->ID, 'thumbnail');
+
+	// Map Coords
+	$gmap  = get_user_meta($user->ID, 'gmap_coords', true); 
+	$map_coords = explode(",", $gmap);
+	$gmap_lat = $map_coords[0];
+	$gmap_lon = $map_coords[1]; 
+
+	//do_shortcode('[show_homepage_map]'); 
+	//echo "<script> draw_single_map('$institution', '".get_user_meta($user->ID, 'billing_address_1', true)."','".get_user_meta($user->ID, 'billing_state', true)."','{$user->user_email}' ,'$gmap_lat', '$gmap_lon', 'Manufacturer'); </script>";  
+
+}?> 
+
 	<?php if(is_search()){?>
 		<!-- Search Map -->
 		<div class="map-container" id="map-container" style="height:400px; width:100%; margin-top:-110px; margin-bottom: 29px;"></div>
@@ -24,14 +58,44 @@ get_header('shop'); ?>
 		<div id="group_search_result" style="width:300px; height:200px; background-color:pink;z-index: 10000;margin-top: -319px;position: absolute;float: right;margin-left: 80%;">
 		</div>-->
 	<?php } ?>
+
 	<?php
 		/**
-		 * woocommerce_before_main_content hook
+		 * woocommerce_before_main_content hook 
 		 *
 		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
 		 * @hooked woocommerce_breadcrumb - 20
-		 */
+		 */ 
+
 		do_action('woocommerce_before_main_content');
+
+		global $wpdb;
+
+		$args = $args = array(
+			'show_option_all'    => '',
+			'show_option_none'   => '',
+			'option_none_value'  => '-1',
+			'orderby'            => 'NAME', 
+			'order'              => 'ASC',
+			'show_count'         => 1,
+			'hide_empty'         => 1, 
+			'child_of'           => 0,
+			'exclude'            => '',
+			'include'			 => '',
+			'echo'               => 1,
+			'selected'           => 0,
+			'hierarchical'       => 0, 
+			'name'               => 'dist_category',
+			'id'                 => 'dist_category',
+			'class'              => 'postform',
+			'depth'              => 0,
+			'tab_index'          => 0,
+			'taxonomy'           => 'category',
+			'hide_if_empty'      => true,
+			'value_field'	     => 'term_id',	
+		);
+		wp_dropdown_categories( $args ); 
+
 	?>
 
 		<?php do_action( 'woocommerce_archive_description' ); ?>
@@ -75,7 +139,6 @@ get_header('shop'); ?>
             
              <?php do_shortcode('[gdym_didyoumean]');?>
              
-
 		<?php endif; ?>
 
 	<?php
@@ -100,6 +163,35 @@ get_header('shop'); ?>
 		 * @hooked woocommerce_get_sidebar - 10
 		 */
 		do_action('woocommerce_sidebar');
-	?>
 
-<?php get_footer('shop'); ?>
+		// global $wpdb;
+
+		// if($manu_slug = ''){
+		if(1==2){
+			
+			$manufacturers_dist = $wpdb->get_results("SELECT w.ID FROM `wp_users` as w INNER JOIN wp_usermeta as m on w.ID = m.user_id WHERE m.meta_key = 'primary_distributor' and m.meta_value!=''"); ?>
+
+			<p align="center" style="padding-top:20px; cursor: pointer; margin-left:auto; margin-right:auto"><a class="button" onclick="jQuery('#show_all_cat').toggle('slow',function(){})" >View All Distributors by <?php echo strtoupper($manu_slug);?></a></p>
+			<div class="dist_manufacturer responsive" style="display:block;"> 
+			    <ul style="width:80%; height:auto; list-style: none; overflow-y:scroll; display:block; margin-top: 30px; margin-bottom: 30px; margin-left:auto; margin-right:auto; list-decoration:none" class="responsive" id="show_all_cat">
+			    <?php 
+			      foreach ($manufacturers_dist as $key => $_user) { 
+			        $user = get_user_by('id', $_user->ID);
+			        if(in_array('shop_manager', $user->roles ) ) { 
+			          $dist = substr(get_user_meta($_user->ID,'primary_distributor',true), 0, -6);
+			        ?>   
+			        <li>
+				        <a target="_blank" href="<?php echo home_url('/vendor/'.$dist.'/?manufacturer='.$manu_slug);?>" >
+				          <span style="border: 2px solid rgb(242, 242, 242); padding: 5px; margin: 5px; float: left">
+				            <?php echo get_user_meta($_user->ID,'institution',true);?>
+				          </span>  
+				        </a>
+				    </li>
+			    <?php  
+			        }
+			      }?> 
+			    </ul> 
+			</div>
+		<?php
+		}  
+get_footer('shop'); ?>
